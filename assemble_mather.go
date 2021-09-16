bbba31cd3ea006934291e2197f64457ae9ea046d
kumu <liubo@zhuangdianwenhua.com>
2021-09-16 21:08:33 +0800

    add match service

--- assemble_mather.go
+++ assemble_mather.go
@@ -16,12 +16,18 @@ type rule struct {
 //match ####
 func (r *rule) match(text string) bool {
 	if r.optType == AND {
+		index := -1
+		//A|B|C 有大概的顺序关系，在包含多个ABC的情况下，可能不准
 		for _, w := range r.words {
-			if !strings.Contains(text, w) {
+			last := strings.Index(text, w)
+			if last > 0 && last > index {
+				index = last
+				continue
+			} else {
 				return false
 			}
 		}
-		return false
+		return true
 	} else if r.optType == NON {
 		if strings.Contains(text, r.words[0]) {
 			for i := 1; i < len(r.words); i++ {
@@ -45,24 +51,28 @@ func NewAssembleMather() *AssembleMather {
 }
 
 func (a *AssembleMather) Build(words []string) {
-	sep := "#"
-	optType := NON
 
 	for _, w := range words {
+
 		if strings.Contains(w, "|") {
-			sep = "|"
-			optType = AND
+			a.rules = append(a.rules, &rule{
+				metaData: w,
+				words:    strings.Split(w, "|"),
+				optType:  AND,
+			})
+		} else if strings.Contains(w, "#") {
+			a.rules = append(a.rules, &rule{
+				metaData: w,
+				words:    strings.Split(w, "#"),
+				optType:  NON,
+			})
 		}
-		a.rules = append(a.rules, &rule{
-			metaData: w,
-			words:    strings.Split(w, sep),
-			optType:  optType,
-		})
 	}
 }
 
 func (a *AssembleMather) Match(text string, repl rune) (word []string, desensitization string) {
-	//命中一条规则就返回
+	//命中一条规则就返回, 不支持脱敏处理；
+	desensitization = text
 	for _, rule := range a.rules {
 		if rule.match(text) {
 			word = append(word, rule.metaData)
---
+++ match_service.go
@@ -0,0 +1,65 @@
+package sensitive_words_match
+
+import "strings"
+
+const (
+	DFA      = 0
+	ASSEMBLE = 1
+	REGEXP   = 2
+)
+
+type MatchService struct {
+	matchers map[int]Matcher
+}
+
+func NewMatchService() *MatchService {
+	return &MatchService{
+		matchers: make(map[int]Matcher),
+	}
+}
+
+//Build 同一个配置中支持三种配置
+func (m *MatchService) Build(words []string) {
+	var (
+		dfaList      []string
+		assembleList []string
+		regexpList   []string
+	)
+
+	for i := 0; i < len(words); i++ {
+		if strings.HasPrefix(words[i], "reg@") {
+			regexpList = append(regexpList, words[i][len("reg@"):])
+		} else if strings.Contains(words[i], "|") || strings.Contains(words[i], "#") {
+			assembleList = append(assembleList, words[i])
+		} else {
+			dfaList = append(dfaList, words[i])
+		}
+	}
+
+	if len(dfaList) > 0 {
+		matcher := NewDFAMather()
+		matcher.Build(dfaList)
+		m.matchers[DFA] = matcher
+	}
+
+	if len(assembleList) > 0 {
+		matcher := NewAssembleMather()
+		matcher.Build(assembleList)
+		m.matchers[ASSEMBLE] = matcher
+	}
+
+	if len(regexpList) > 0 {
+		//todo: 暂不支持
+	}
+}
+
+//Match 只要有一个规则就返回；
+func (m *MatchService) Match(text string, repl rune) (sensitiveWords []string, replaceText string) {
+	for _, x := range m.matchers {
+		sensitiveWords, replaceText = x.Match(text, '*')
+		if len(sensitiveWords) > 0 {
+			return
+		}
+	}
+	return
+}
